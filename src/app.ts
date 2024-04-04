5aa549522ab67587eaa9c3230bad4876d57a2fbe
Probir Sarkar <Probirsarkar855@gmail.com>
2024-04-04 15:02:12 +0530

    added routes for query

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,18 @@ import {errorHandler} from '@/middlewares/errorHandler';
 
 const app: Express = express();
 
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send('Hello World');
-});
+
+app.get('/', (req, res) => res.send('Hello World'));
 
 app.route('/contacts').get(getContacts).post(createContact)
 
 
+// Route not found handler, must be the last route and before the global error handler, It will handle all routes that are not found
 app.all('*', (req, res) => res.status(404).json({ message: 'Route not found' }));
 
+// Global error handler, must be the last middleware, It will handle all errors
 app.use(errorHandler);
 
 app.listen(env.port, () => {
--- src/controllers/contacts.ts
+++ src/controllers/contacts.ts
@@ -1,6 +1,7 @@
 import { RequestHandler } from "express";
 import prisma from "@/configs/prisma";
 import z from "zod";
+import { formatZodErrors } from "@/utils";
 
 export const getContacts: RequestHandler = async (req, res) => {
   try {
@@ -30,6 +31,8 @@ const createContactSchema = z.object({
 
 export const createContact: RequestHandler = async (req, res) => {
   try {
+    console.log(req.body);
+    
     const { address, ...contactData } = createContactSchema.parse(req.body);
     const exisingContact = await prisma.contact.findUnique({
       where: {
@@ -59,8 +62,56 @@ export const createContact: RequestHandler = async (req, res) => {
     return res.status(500).json({ message: "Failed to create contact" });
   } catch (error) {
     if (error instanceof z.ZodError) {
-      return res.status(400).json({ message: "Invalid data", errors: error.errors });
+      return res.status(400).json({ message: "Invalid data", errors: formatZodErrors(error)});
     }
     return res.status(500).json({ message: "Internal server error" });
   }
 };
+
+export const updateContact: RequestHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if(!id || isNaN(parseInt(id))) return res.status(400).json({ message: "Invalid contact id" });
+    const { address, ...contactData } = createContactSchema.parse(req.body);
+    const contact = await prisma.contact.update({
+      where: {
+        id: parseInt(id),
+      },
+      data: {
+        ...contactData,
+        address: {
+          update: {
+            ...address,
+          },
+        },
+      },
+    });
+    if (contact) {
+      return res.status(200).json(contact);
+    }
+    return res.status(500).json({ message: "Failed to update contact" });
+  } catch (error) {
+    if (error instanceof z.ZodError) {
+      return res.status(400).json({ message: "Invalid data", errors: formatZodErrors(error)});
+    }
+    return res.status(500).json({ message: "Internal server error" });
+  }
+};
+
+export const deleteContact: RequestHandler = async (req, res) => {
+  try {
+    const { id } = req.params;
+    if(!id || isNaN(parseInt(id))) return res.status(400).json({ message: "Invalid contact id" });
+    const contact = await prisma.contact.delete({
+      where: {
+        id: parseInt(id),
+      },
+    });
+    if (contact) {
+      return res.status(200).json({ message: "Contact deleted successfully" });
+    }
+    return res.status(500).json({ message: "Failed to delete contact" });
+  } catch (error) {
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
