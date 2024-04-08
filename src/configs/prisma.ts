b3a9e6fbde0422aafa33d81e8cb1c71942ee82f6
Probir Sarkar <Probirsarkar855@gmail.com>
2024-04-08 15:49:43 +0530

    need to move drizzle

--- src/configs/prisma.ts
+++ src/configs/prisma.ts
@@ -1,5 +1,16 @@
+import { env, nextTick } from "node:process";
+
+import { Pool, neonConfig } from "@neondatabase/serverless";
+import { PrismaNeon } from "@prisma/adapter-neon";
 import { PrismaClient } from "@prisma/client";
 
-const prisma = new PrismaClient();
+import ws from "ws";
+
+neonConfig.webSocketConstructor = ws;
+const connectionString = `${env.DATABASE_URL}`;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaNeon(pool);
+const prisma = new PrismaClient({ adapter });
 
 export default prisma;
